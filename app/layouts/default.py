db2eef7cb6bb325a633babb53ca9e377cb1c6dd1
krashmello <joelenrikesoteldo@gmail.com>
2025-10-16 09:57:10 -0400

    feat: update layout component textarea

--- app/layouts/default.py
+++ app/layouts/default.py
@@ -2,6 +2,7 @@ from textual.app import ComposeResult
 from textual.containers import Horizontal, Vertical, VerticalScroll, Container, Grid
 from textual.widgets import Static
 from app.components.filesTree import KmDirectoryTree
+from app.components.textArea import KmTextArea
 from textual.widgets import Markdown, TextArea
 
 def compose(self) -> ComposeResult:
@@ -12,7 +13,6 @@ def compose(self) -> ComposeResult:
                 markdown = Markdown(id='preview', classes="markdown_preview")
                 markdown.code_indent_guides = False
                 yield markdown
-                text_area = TextArea(language='markdown', classes="markdown_text")
+                text_area = KmTextArea(language='markdown', classes="markdown_text", id='editor')
                 yield text_area
-    
 
