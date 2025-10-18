4c9b2ee46f54bfe699acd5174391496b4ea7dd96
KrashMello <joelenrikesoteldo@gmail.com>
2025-10-18 19:41:45 -0400

    feat: update component layout

--- app/application.py
+++ app/application.py
@@ -1,18 +1,17 @@
 from textual.app import App
 from . import layouts
 from textual.widgets import Markdown, TextArea
+from app.layouts.default import DefaultLayout
+
 
 class application(App):
-
     CSS_PATH = "./assets/css/utility_containers.tcss"
     file_path = None
 
     def compose(self):
-        yield from layouts.default.compose(self)
+        yield DefaultLayout(id="layout")
 
     async def on_text_area_changed(self, event: TextArea.Changed) -> None:
         # Actualiza el widget Markdown según lo que se escribe
         preview = self.query_one("#preview", Markdown)
         preview.update(f"{event.text_area.text}")
-
-
